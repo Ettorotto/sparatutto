2f114a213a694e00d56f628d609d335b89684684
Ettore <ettore.paolo.2008@gmail.com>
2026-03-14 17:06:52 +0100

    created reposetory
    took the files from the other folder and put them here in this new folder

---
+++ script.js
@@ -0,0 +1,88 @@
+/**
+ * TODO: voglio fare che ce 1 possibilita su 10 che il target sia dorato, e che quando cliccato da ben 10 punti! pero hai solo 600ms per cliccarlo!
+ */
+    let target = document.getElementById("target");
+    let punti = document.getElementById("punti");
+    let tempo = document.getElementById("tempo");
+    
+    let recordEl = document.getElementById("record");
+    let record = Infinity; // best time in ms (lower is better). Infinity means "no record yet"
+    //ok per questo o chiesto a una chatIA perche non apivo perche il record non funzionasse. 
+    //? mi ha detto di separare record come elemento dall'HTML e record come numero per farlo funzionare, io stavo facendo tutto con una sola variabile... daje impariamo sempre cose nuove
+
+    let punteggio = 0;
+    let tempo_tra_spostamenti = prompt("inserisci in millisecondi la velocita del bersaglio");
+    let punti_per_target=1
+
+    target.addEventListener("click", spostalo);
+    target.addEventListener("click", segnaPunto);
+
+    let spostamento_programamto = setInterval(spostalo,tempo_tra_spostamenti ); //set interVal fara la funzione spostalo ogni 2000 millisecondi (2 secondi)
+
+    function spostalo()
+    {
+        
+
+        let w = window.innerWidth-100;
+        let h = window.innerHeight-100;
+
+        let x = Math.random() * w;
+        let y = Math.random() * h;
+
+        target.style.left = x+"px";
+        target.style.top=y+"px";
+
+        let rarita = Math.random();
+        
+        if(rarita<=0.1){
+            target.style.color = "yellow";
+            punti_per_target = 10;
+        }
+
+        console.log(x, ", ",y);
+    }
+
+    //funzione che si avvia ogni volta che l'utente prende il bersaglio
+    function segnaPunto(){
+
+        tempo.innerHTML = "Tempo: " + Cronometro + "ms";
+
+        
+        if (Cronometro < record) {
+            record = Cronometro;
+            recordEl.innerHTML = "Record: " + Cronometro + "ms";
+        }
+
+        punteggio += punti_per_target;   // assegno il punto e lo scrivo
+        punti.innerHTML = "Punti : " + punteggio;
+
+        clearInterval(spostamento_programamto);
+        spostamento_programamto = setInterval(spostalo,tempo_tra_spostamenti);
+
+        Cronometro_reset();
+
+        punti_per_target = 1;
+
+        console.log("PUNTO-" + Cronometro + " - " + record);
+    }
+
+    /**
+     * adesso faccio un intervallo che si ripete ogni millisecondo per avere un cronometro
+     * che chiama sempre il metodo per aggiungere un millisecondo al cronometro... forse esistono metodi migliore ma cosi va bene.
+     * e un metodo per resettare il cronometro
+    */
+
+   let Cronometro = 0;
+
+    setInterval(Cronometro_plus,1);
+
+    function Cronometro_plus(){
+        Cronometro++;
+    }
+
+    function Cronometro_reset(){
+        Cronometro=0;
+    }
+
+
+    
